f53beea77617d6058066fc3a1de4663efd3fbfbf
Pedro Henrique Braga <prometeuslbo@gmail.com>
2023-04-27 16:08:59 -0300

    Teste

---
+++ eda/cpp_programs/exerciciosRecursividade/dois.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*
+    Implemente uma função recursiva que verifique se duas strings são iguais (retorne 1) ou 
+    não (retorne 0).
+        *Se duas strings são vazias, elas são iguais;
+        *Se todos os seus caracteres são iguais, elas são iguais.
+    -----------------------------------------------------------------------------------------------
+    A comparação segue as seguintes etapas:
+        1° -> Aferir se as strings possuem o mesmo tamanho, caso não retorne 0;
+        2° -> Caso possuam o mesmo tamanho, fazer a comparação caractere a caractere, começando do 
+        ultimo char até o primeiro
+            2.1° -> condição de parada
+            2.2° -> chamada recursiva
+*/
+int strings_iguais(char * string_um, char * string_dois, int string_um_tamanho, int string_dois_tamanho ){
+
+}
+
+int main(){
+
+    return 0;
+}
