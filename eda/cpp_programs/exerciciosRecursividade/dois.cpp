ec157143a1df0edfe9583c3d8316f443c4cf08e9
Pedro Henrique Braga <prometeuslbo@gmail.com>
2023-04-28 00:06:34 -0300

    Adicionando a resolucao da questao2

--- eda/cpp_programs/exerciciosRecursividade/dois.cpp
+++ eda/cpp_programs/exerciciosRecursividade/dois.cpp
@@ -13,11 +13,33 @@ using namespace std;
             2.1° -> condição de parada
             2.2° -> chamada recursiva
 */
-int strings_iguais(char * string_um, char * string_dois, int string_um_tamanho, int string_dois_tamanho ){
-
+int strings_iguais(char * string_um, char * string_dois){
+    if(string_um == NULL && string_dois == NULL){
+        //condicao de parada
+        //Caso as string estejam vazias(Util no primeiro instante da recursividade)
+        return 1;
+    }
+    else if(*string_um != *string_dois){
+        //condicao de parada
+        //Caso o caractere da string_um for diferente do caractere da string_dois
+        return 0;
+    } else if(*string_um == 0 && *string_dois == 0){
+        //condicao de parada
+        /*Caso o ponteiro das duas string atinja EOF, significa que em todas as outras chamadas, 
+        os caracteres eram iguais.*/
+        return 1;
+    } else{
+        return strings_iguais((string_um + 1),(string_dois + 1));
+    }
 }
 
 int main(){
-
+    char string_um[4] = "Ola";
+    char string_dois[4] = "Ola";
+    char string_tres[4] = "OLA";
+    string resp1 = (strings_iguais(string_um,string_dois))? "Sim" : "Nao";
+    string resp2 = (strings_iguais(string_um,string_tres)) ? "Sim":"Nao";
+    cout << "String_um e String_dois sao iguais? " << resp1 << endl;
+    cout << "String_um e String_tres sao iguais? " << resp2 << endl;
     return 0;
 }
